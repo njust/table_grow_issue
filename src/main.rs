3a9ab7eaa85aa0b1f205f9e38d22eb6d4bba57cf
Nico Just <mail@njust.de>
2022-12-13 15:42:44 +0100

    Updated example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::{egui, Theme};
 use eframe::egui::{Align, Layout};
-use egui_extras::{Size, TableBuilder};
+use eframe::{egui, Theme};
 
+// use egui_extras::{Size, TableBuilder};
+use egui_extras::{Column, TableBuilder};
 fn main() {
-    let options = eframe::NativeOptions{
+    let options = eframe::NativeOptions {
         default_theme: Theme::Light,
+        initial_window_size: Some([800., 900.].into()),
         ..eframe::NativeOptions::default()
     };
     eframe::run_native(
@@ -24,8 +26,8 @@ struct MyApp {
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            cnt: 10,
-            data: vec![]
+            cnt: 100,
+            data: vec![],
         }
     }
 }
@@ -37,26 +39,6 @@ impl eframe::App for MyApp {
             .default_width(150.)
             .resizable(true)
             .show(ctx, |ui| {
-                TableBuilder::new(ui)
-                    .cell_layout(Layout::left_to_right(Align::Center))
-                    .column(Size::remainder())
-                    .column(Size::exact(30.))
-                    .striped(true)
-                    .body(|body| {
-                        body.rows(20., self.data.len(), |idx, mut row| {
-                            let item = self.data.get(idx).expect("Invalid idx");
-                            row.col(|col| {
-                               col.label(item);
-                            });
-
-                            row.col(|col| {
-                                col.label("item");
-                            });
-                        })
-                    });
-            });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
             ui.add(egui::Slider::new(&mut self.cnt, 1..=200));
             if ui.button("Add long text").clicked() {
                 for i in 0..self.cnt {
@@ -73,6 +55,28 @@ impl eframe::App for MyApp {
             if ui.button("Clear").clicked() {
                 self.data.clear();
             }
+            });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            TableBuilder::new(ui)
+                .cell_layout(Layout::left_to_right(Align::Center))
+                .column(Column::auto().clip(true))
+                .column(Column::auto().clip(true))
+                .striped(true)
+                // .column(Size::relative(0.5))
+                // .column(Size::relative(0.5))
+                .body(|body| {
+                    body.rows(20., self.data.len(), |idx, mut row| {
+                        let item = self.data.get(idx).expect("Invalid idx");
+                        row.col(|col| {
+                            col.label(item);
+                        });
+
+                        row.col(|col| {
+                            col.label("item");
+                        });
+                    })
+                });
         });
     }
 }
